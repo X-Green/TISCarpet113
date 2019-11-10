3279582d8e02080055db786bd081d6d066148d1d
Fallen_Breath <yqx791125833@gmail.com>
2019-11-10 23:31:12 +0800

    update block updates logger
    * now use wool block to specialize observer
    + colorful log message based on wool's color
    + activator rail on wool block can log powered state changes

--- src/main/java/carpet/logging/logHelpers/BlockUpdatesLogHelper.java
+++ src/main/java/carpet/logging/logHelpers/BlockUpdatesLogHelper.java
@@ -12,6 +12,8 @@ import carpet.logging.LoggerRegistry;
 import carpet.utils.Messenger;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.NextTickListEntry;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.item.EnumDyeColor;
 import net.minecraft.util.math.BlockPos;
 
 public class BlockUpdatesLogHelper {
@@ -51,10 +53,36 @@ public class BlockUpdatesLogHelper {
         BlockUpdatesLogHelper.gameTime = gameTime;
     }
     
-    public static void onBlockUpdated(BlockPos pos, String type, int dimensionID)
+    private static String getColorStype(EnumDyeColor color)
+    {
+        switch (color)
+        {
+            case WHITE: return "w";
+            case ORANGE: return "d";
+            case MAGENTA: return "m";
+            case LIGHT_BLUE: return "c";
+            case YELLOW: return "y";
+            case LIME: return "l";
+            case PINK: return "r";
+            case GRAY: return "f";
+            case LIGHT_GRAY: return "g";
+            case CYAN: return "q";
+            case PURPLE: return "p";
+            case BLUE: return "v";
+            case BROWN: return "n";
+            case GREEN: return "e";
+            case RED: return "r";
+            case BLACK: return "k";
+        }
+        return "t";
+    }
+    
+    public static void onBlockUpdated(BlockPos pos, String type, int dimensionID, EnumDyeColor color)
     {
         BlockUpdatesLogHelper.messages.add(Messenger.c(
-                "w (" + pos.getX() + "," + pos.getY() + "," + pos.getZ() + ") ",
+                BlockUpdatesLogHelper.getColorStype(color) + " # ",
+                Messenger.tp("w", pos),
+                "w  ",
                 "t " + type + " ",
                 "g at ",
                 "y " + BlockUpdatesLogHelper.stage + " ",
@@ -73,7 +101,7 @@ public class BlockUpdatesLogHelper {
         LoggerRegistry.getLogger("blockupdates").log( (option) -> 
         {
             List<ITextComponent> comp = new ArrayList<>();
-            comp.add(Messenger.c("w [GameTime " + gameTime + "] ---------------------------"));
+            comp.add(Messenger.c("g [GameTime ", "w " + gameTime, "g ] ---------------------------"));
             while (iterator.hasNext()) 
             {
                 ITextComponent message = iterator.next();
